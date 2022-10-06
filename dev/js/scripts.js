7f246481a3dfff08acb55ed2ba082d93810da31a
davidfether <102702088+davidfether@users.noreply.github.com>
2022-10-06 11:19:03 -0400

    week 3 in class

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -1,8 +1,17 @@
 import {
     gsap
 } from "gsap";
-gsap.from("#header", {
-    duration: 0.5,
-    x: -300,
-    alpha: 0
+
+console.log(gsap)
+
+gsap.from("header", {
+    duration: 0.25,
+    alpha: 0,
+    y: -300
+});
+
+gsap.from("ticket-door", {
+    duration: 0.25,
+    alpha: 0,
+    x: -100
 });
