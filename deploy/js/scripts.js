1e09ea69d9b4b09599db24bfcb2c93640f28473c
davidfether <102702088+davidfether@users.noreply.github.com>
2022-10-09 21:27:35 -0400

    js finished

--- deploy/js/scripts.js
+++ deploy/js/scripts.js
@@ -1,3 +1,6 @@
 import { gsap } from "gsap";
 
-gsap.from("#header", {duration: 0.25, x: -200, alpha: 0});
+gsap.from("#header",{duration:0.6,x:200,alpha:0,delay:0.25});
+gsap.from("#line-2",{duration:0.75,x:200,alpha:0,delay:0.75});
+
+gsap.from("#button",{duration:0.75,y:100,alpha:0,delay:0.75});
