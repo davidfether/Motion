1faaa8fab5a7b6724ef3583b9633651016ce0f6f
davidfether <102702088+davidfether@users.noreply.github.com>
2022-10-10 22:42:40 -0400

    monday-timeline

--- deploy/js/scripts.js
+++ deploy/js/scripts.js
@@ -4,17 +4,31 @@ import { gsap } from "gsap";
 
 function heroanimation(){
     var tl = gsap.timeline();
-    tl.from("#header",{duration:0.4,x:200,alpha:0,delay:0.25})
-        .from("#line-2",{duration:0.3,x:200,alpha:0,delay:0.15})
-
-        .from("#button",{duration:0.3,y:50,alpha:0,delay:0.25})
-return tl;
+    tl.from("#header",{duration:0.75,x:200,alpha:0,delay:0.25})
+        .from("#line-2",{duration:0.5,x:200,alpha:0,delay:1},"-0.25")
+        .from("#button",{duration:0.5,y:50,alpha:0,delay:1},"-0.25");
+return tl; 
 }
-
-
 
 var mainTL = gsap.timeline(); 
 mainTL.add(heroanimation()); 
+
+let button = document.querySelector("aside");
+
+var buttonTL = gsap.timeline({paused:true});
+buttonTL.to("#button",{duration:0.25})
+    .to("aside",{duration:0.25,x:-800},"playSameTime")
+    .to("form",{duration:0.25,scaleX:1},"playSameTime")
+    .to("aside",{duration:0.25,alpha:0},"playSameTime");
+
+button.addEventListener("mouseenter", function(){
+    buttonTL.play();
+})
+
+    button.addEventListener("mouseleave", function(){
+        buttonTL.reverse();
+})
+
 
 
 
