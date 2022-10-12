fa5a156b11aa6b06cd7d88ec2943399c55ed9201
davidfether <102702088+davidfether@users.noreply.github.com>
2022-10-12 19:15:28 -0400

    altering timeline and folders wed

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -6,13 +6,16 @@ function heroAnimation(){
     var tl = gsap.timeline();
     let mm = gsap.matchMedia();
 
+
     mm.add("(min-width 760px)",() => {
+        //desktop setup code here...
       tl.from("#header",{duration:0.75,x:200,alpha:0})
         .from("#line-2",{duration:0.5,x:200,alpha:0},"-0.25")
         .from("#button",{duration:0.5,y:50,alpha:0},"-0.25");
     });
 
     mm.add("(max-width 759px)",() => {
+          //desktop setup code here...
      tl.from("#header",{duration:0.75,y:200,alpha:0})
         .from("#line-2",{duration:0.5,y:200,alpha:0},"same")
         .from("#button",{duration:0.5,y :50,alpha:0},"same");
