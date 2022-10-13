66f110be0f3e4d2b4e10a8e3f84f3ac3bfc0244f
davidfether <102702088+davidfether@users.noreply.github.com>
2022-10-12 21:02:44 -0400

    gsap working, mouseover not working

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -4,19 +4,18 @@ function heroAnimation(){
     var tl = gsap.timeline();
     let mm = gsap.matchMedia();
 
-    mm.add("(min-width 760px)",() => {
+    mm.add("(min-width: 800px)", () => {
+        // desktop setup code here...
       tl.from("#header",{duration:0.75,x:200,alpha:0})
         .from("#line-2",{duration:0.5,x:200,alpha:0},"-0.25")
         .from("#button",{duration:0.5,y:50,alpha:0},"-0.25");
-    });
 
-    mm.add("(max-width 759px)",() => {
+    mm.add("(max-width: 799px)", () => {
+        // mobile setup code here...
      tl.from("#header",{duration:0.75,y:200,alpha:0})
         .from("#line-2",{duration:0.5,y:200,alpha:0},"same")
         .from("#button",{duration:0.5,y :50,alpha:0},"same");
-    });
-
-    return tl; 
+    return tl;
 }
 
 var mainTL = gsap.timeline(); 
@@ -32,12 +31,10 @@ button.addEventListener("mouseenter", function(){
     buttonTL.to("#button,", {duration:0.3, scale: 1.5})
     .to("#line-2", {duration: 0.25, alpha:1, y:50})
     .to("#header", {duration: 0.25, alpha:1, y:20});
-})
-
-
+    
 button.addEventListener("mouseleave", function(){
     buttonTL.reverse();
     buttonTL.to("#button,", {duration:0.3, scale: 1.5})
     .to("#line-2", {duration: 0.25, alpha:1, y:0})
     .to("#header", {duration: 0.25, alpha:1, y:0});
-})
+});
