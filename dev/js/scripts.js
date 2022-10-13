a3b88e592e37b2ab06781d220f741ce006205ca9
davidfether <102702088+davidfether@users.noreply.github.com>
2022-10-12 20:41:50 -0400

    updated event listener-wed

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -1,21 +1,16 @@
 import { gsap } from "gsap";
 
-gsap.set("form"),{scaleX:0, transformOrigin:"left center"};
-
 function heroAnimation(){
     var tl = gsap.timeline();
     let mm = gsap.matchMedia();
 
-
     mm.add("(min-width 760px)",() => {
-        //desktop setup code here...
       tl.from("#header",{duration:0.75,x:200,alpha:0})
         .from("#line-2",{duration:0.5,x:200,alpha:0},"-0.25")
         .from("#button",{duration:0.5,y:50,alpha:0},"-0.25");
     });
 
     mm.add("(max-width 759px)",() => {
-          //desktop setup code here...
      tl.from("#header",{duration:0.75,y:200,alpha:0})
         .from("#line-2",{duration:0.5,y:200,alpha:0},"same")
         .from("#button",{duration:0.5,y :50,alpha:0},"same");
@@ -28,16 +23,21 @@ var mainTL = gsap.timeline();
     mainTL.add(heroAnimation());
 
 
-let button = document.querySelector("scale");
+let button = document.querySelector('button');
 
 var buttonTL= gsap.timeline({paused:true});
-buttonTL.to("#button",{duration:0.25})
-    .to("scale",{duration:0.25,scale:10},"playSameTime");
 
 button.addEventListener("mouseenter", function(){
     buttonTL.play();
+    buttonTL.to("#button,", {duration:0.3, scale: 1.5})
+    .to("#line-2", {duration: 0.25, alpha:1, y:50})
+    .to("#header", {duration: 0.25, alpha:1, y:20});
 })
- 
-    button.addEventListener("mouseleave", function(){
-        buttonTL.reverse();
-});
+
+
+button.addEventListener("mouseleave", function(){
+    buttonTL.reverse();
+    buttonTL.to("#button,", {duration:0.3, scale: 1.5})
+    .to("#line-2", {duration: 0.25, alpha:1, y:0})
+    .to("#header", {duration: 0.25, alpha:1, y:0});
+})
