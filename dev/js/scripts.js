56251c697aaf41837f5e11eee1dea7299db9da62
davidfether <102702088+davidfether@users.noreply.github.com>
2022-10-16 18:30:20 -0400

    firesidechat-timeline-changes

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -1,5 +1,7 @@
 import { gsap } from "gsap";
 
+var mainTL = gsap.timeline(); 
+
 function heroAnimation(){
     var tl = gsap.timeline();
     let mm = gsap.matchMedia();
@@ -7,34 +9,33 @@ function heroAnimation(){
     mm.add("(min-width: 800px)", () => {
         // desktop setup code here...
       tl.from("#header",{duration:0.75,x:200,alpha:0})
-        .from("#line-2",{duration:0.5,x:200,alpha:0},"-0.25")
-        .from("#button",{duration:0.5,y:50,alpha:0},"-0.25");
+        .from("#line-2",{duration:0.5,x:200,alpha:0},"same")
+        .from("#hero-btn",{duration:0.5,y:50,alpha:0},"same");
+    });
 
     mm.add("(max-width: 799px)", () => {
         // mobile setup code here...
      tl.from("#header",{duration:0.75,y:200,alpha:0})
         .from("#line-2",{duration:0.5,y:200,alpha:0},"same")
-        .from("#button",{duration:0.5,y :50,alpha:0},"same");
+        .from("#hero-btn",{duration:0.5,y :50,alpha:0},"same");
     return tl;
+    });
 }
 
-var mainTL = gsap.timeline(); 
-    mainTL.add(heroAnimation());
+mainTL.add(heroAnimation());
 
-
-let button = document.querySelector('button');
+// let button = document.querySelector('#hero-btn');
+var heroBTn = document.querySelector("#hero-btn");
 
 var buttonTL= gsap.timeline({paused:true});
+buttonTL.to("#hero-btn", {duration:0.3, scale: 1.5})
+    .to("#line-2", {duration: 0.25, alpha:0, y:50})
+    .to("#header", {duration: 0.25, alpha:0, y:20});
 
-button.addEventListener("mouseenter", function(){
+heroBTn.addEventListener("mouseenter", function(){
     buttonTL.play();
-    buttonTL.to("#button,", {duration:0.3, scale: 1.5})
-    .to("#line-2", {duration: 0.25, alpha:1, y:50})
-    .to("#header", {duration: 0.25, alpha:1, y:20});
-    
-button.addEventListener("mouseleave", function(){
+})
+
+heroBTn.addEventListener("mouseleave", function(){
     buttonTL.reverse();
-    buttonTL.to("#button,", {duration:0.3, scale: 1.5})
-    .to("#line-2", {duration: 0.25, alpha:1, y:0})
-    .to("#header", {duration: 0.25, alpha:1, y:0});
-});
+})
