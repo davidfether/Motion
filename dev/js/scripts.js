800285dd08f37cc8341d757d89a7f999cca2a520
davidfether <102702088+davidfether@users.noreply.github.com>
2022-10-17 19:57:07 -0400

    timeline code added

---
+++ dev/js/scripts.js
@@ -0,0 +1,41 @@
+import { gsap } from "gsap";
+
+var mainTL = gsap.timeline(); 
+
+function heroAnimation(){
+    var tl = gsap.timeline();
+    let mm = gsap.matchMedia();
+
+    mm.add("(min-width: 800px)", () => {
+        // desktop setup code here...
+      tl.from("#header",{duration:0.75,x:200,alpha:0})
+        .from("#line-2",{duration:0.5,x:200,alpha:0},"same")
+        .from("#hero-btn",{duration:0.5,y:50,alpha:0},"same");
+    });
+
+    mm.add("(max-width: 799px)", () => {
+        // mobile setup code here...
+     tl.from("#header",{duration:0.75,y:200,alpha:0})
+        .from("#line-2",{duration:0.5,y:200,alpha:0},"same")
+        .from("#hero-btn",{duration:0.5,y :50,alpha:0},"same");
+    return tl;
+    });
+}
+
+mainTL.add(heroAnimation());
+
+// let button = document.querySelector('#hero-btn');
+var heroBTn = document.querySelector("#hero-btn");
+
+var buttonTL= gsap.timeline({paused:true});
+buttonTL.to("#hero-btn", {duration:0.3, scale: 1.5})
+    .to("#line-2", {duration: 0.25, alpha:0, y:50})
+    .to("#header", {duration: 0.25, alpha:0, y:20});
+
+heroBTn.addEventListener("mouseenter", function(){
+    buttonTL.play();
+})
+
+heroBTn.addEventListener("mouseleave", function(){
+    buttonTL.reverse();
+})
