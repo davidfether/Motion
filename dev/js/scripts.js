968df52f093affeeff4175dc4c217d5baf6685bf
davidfether <102702088+davidfether@users.noreply.github.com>
2022-10-17 20:43:04 -0400

    added html

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -1,46 +1,16 @@
-import { gsap } from "gsap";
-import { ScrollTrigger } from "gsap/ScrollTrigger";
+import $ from "jquery";
 
-gsap.registerPlugin(ScrollTrigger);
+let $nav = $('nav[data-nav]');
+let isVisible = false;
 
-gsap.to("")
-
-var mainTL = gsap.timeline(); 
-
-function heroAnimation(){
-    var tl = gsap.timeline();
-    let mm = gsap.matchMedia();
-
-    mm.add("(min-width: 800px)", () => {
-        // desktop setup code here...
-      tl.from("#header",{duration:0.75,x:200,alpha:0})
-        .from("#line-2",{duration:0.5,x:200,alpha:0},"same")
-        .from("#hero-btn",{duration:0.5,y:50,alpha:0},"same");
-    });
-
-    mm.add("(max-width: 799px)", () => {
-        // mobile setup code here...
-     tl.from("#header",{duration:0.75,y:200,alpha:0})
-        .from("#line-2",{duration:0.5,y:200,alpha:0},"same")
-        .from("#hero-btn",{duration:0.5,y :50,alpha:0},"same");
-    return tl;
-    });
-}
-
-mainTL.add(heroAnimation());
-
-// let button = document.querySelector('#hero-btn');
-var heroBTn = document.querySelector("#hero-btn");
-
-var buttonTL= gsap.timeline({paused:true});
-buttonTL.to("#hero-btn", {duration:0.3, scale: 1.5})
-    .to("#line-2", {duration: 0.25, alpha:0, y:50})
-    .to("#header", {duration: 0.25, alpha:0, y:20});
-
-heroBTn.addEventListener("mouseenter", function(){
-    buttonTL.play();
-})
-
-heroBTn.addEventListener("mouseleave", function(){
-    buttonTL.reverse();
+$(".nav-btns").on("click", function(){
+    console.log("click");
+    if(isVisible === false){
+        $nav.show();
+        isVisible = true;
+    }
+    else{
+        $nav.hide();
+        isVisible = false;
+    }
 })
