e58227a8ff3f49e6398051facea3627403b62a73
davidfether <102702088+davidfether@users.noreply.github.com>
2022-10-24 23:57:56 -0400

    final-push-scrolltrigger

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -24,8 +24,8 @@ var mainTl = gsap.timeline();
 
 
 function historyAnimation(){
-    var tl = gsap.timeline({scrollTrigger:{trigger:"skills", scrub: true, markers: false, start:"top 80%", end:"bottom 90%"}});
-    tl.from("#robot hero spot",{ duration: 0.15, scale:5})
+    var tl = gsap.timeline({scrollTrigger:{trigger:"history", scrub: true, markers: false, start:"top 80%, 200px", end:"bottom 90%"}});
+    tl.from("#robot hero spot",{ duration: 0.15, scale:3})
     .from("#history-first",{duration: 0.25, scale:2, y:-400, alpha:-2},"-=0.5") 
     .from("#history-middle",{duration: 0.25, y:-400, alpha:-2},"-=0.5") 
     .from("#history-last",{duration: 0.25, scale:2, y:400, alpha:0},"-=0.5") 
@@ -37,9 +37,13 @@ function historyAnimation(){
 
 
 function skillsAnimation(){
-    var tl = gsap.timeline({scrollTrigger:{trigger:"skills", scrub: true, markers: true, start:"top 70%", end:"bottom 70%"}});
-    tl.from("#skills-content",{ duration: 0.15, x:200},"-=0.5")
-
+    var tl = gsap.timeline({scrollTrigger:{trigger:"skills", scrub: true, markers: true, start:"top 40s%", end:"bottom 40%"}});
+    tl.from("#skills-content",{ duration: 0.25, x:200})
+    tl.from("#top",{ duration: 0.25, stagger:0.5, x:500})
+    tl.from("#emotion",{ duration: 0.2, y:100},"-=0.35")
+    tl.from("#learning",{ duration: 0.2, y:100},"-=0.35")
+    tl.from("#math",{ duration: 0.2, y:100},"-=0.35")
+    tl.from("#hero-2",{ duration: 0.2, alpha:0},"-=0.3")
     return tl;
 }
 
