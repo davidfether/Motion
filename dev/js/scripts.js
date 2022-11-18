38b2d38ad32b0bf69d328328ce1f37a51e2743d2
davidfether <102702088+davidfether@users.noreply.github.com>
2022-11-18 12:52:48 -0500

    gsap-not-working-getting-help

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -4,17 +4,54 @@ import { DrawSVGPlugin } from "gsap/DrawSVGPlugin";
 import { MotionPathPlugin } from "gsap/MotionPathPlugin";
 import { CustomEase } from "gsap/CustomEase";
 
+gsap.set
+
 gsap.registerPlugin(GSDevTools, DrawSVGPlugin, MotionPathPlugin,CustomEase);
 
 function starAnimation(){
     var tl = gsap.timeline();
-    tl.from("#line-1",{duration:0.5, y:"-=500"});
+    tl.from("#line-1",{duration:0.5, y:"-=500", ease:"none"})
+    .to("#line-1",{duration:0.5, scaleY:1.5, ease: "none)"})
     return tl;
 }
 
+
 function eAnimation(){
     var tl = gsap.timeline();
-    tl.to("#letter-e-2",{duration:0.5, y:"-=500"});
+    return tl;
+}
+
+
+function vAnimation(){
+    var tl = gsap.timeline();
+    return tl;
+}
+
+
+
+function oAnimation(){
+    var tl = gsap.timeline();
+    return tl;
+}
+
+
+
+function lAnimation(){
+    var tl = gsap.timeline();
+    return tl;
+}
+
+
+
+function v2Animation(){
+    var tl = gsap.timeline();
+    return tl;
+}
+
+
+
+function e2Animation(){
+    var tl = gsap.timeline();
     return tl;
 }
 
@@ -26,8 +63,8 @@ mainTL.add(starAnimation())
 .add(vAnimation(),"-=0.6")
 .add(oAnimation(),"-=0.35")
 .add(lAnimation(),"-=0.6")
-.add(v2PathAnimation(),"-=0.6")
-.add(e2tAnimation(),"-=0.55")
+.add(v2Animation(),"-=0.6")
+.add(e2Animation(),"-=0.55")
 
 
 GSDevTools.create();
