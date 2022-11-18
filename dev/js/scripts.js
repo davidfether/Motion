7d8158709dcc516f68f9eff7c81d7fa9e4c658f0
davidfether <102702088+davidfether@users.noreply.github.com>
2022-11-18 11:59:09 -0500

    testing-things

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -6,9 +6,16 @@ import { CustomEase } from "gsap/CustomEase";
 
 gsap.registerPlugin(GSDevTools, DrawSVGPlugin, MotionPathPlugin,CustomEase);
 
+function starAnimation(){
+    var tl = gsap.timeline();
+    tl.from("#line-1",{duration:0.5, y:"-=500", ease:"none"})
+    return tl;
+}
+
+
+
 var mainTL = gsap.timeline();
-mainTL.add(tAnimation())
-.add(starAnimation(),"-=0.55")
+mainTL.add(starAnimation())
 .add(eAnimation(),"-=0.25")
 .add(vAnimation(),"-=0.6")
 .add(oAnimation(),"-=0.35")
