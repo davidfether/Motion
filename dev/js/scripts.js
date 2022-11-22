3aa167b226210ae95a2239bfbee408e14037b4df
davidfether <102702088+davidfether@users.noreply.github.com>
2022-11-22 10:09:06 -0500

    in-class-push

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -6,20 +6,22 @@ import { CustomEase } from "gsap/CustomEase";
 
 gsap.registerPlugin(GSDevTools, DrawSVGPlugin, MotionPathPlugin,CustomEase);
 
-gsap.set("#line-1",{transformOrigin: "50% 50%", xPercent:450, yPercent:0, scale:1.5});
-gsap.set("#line-2",{transformOrigin: "50% 50%", xPercent:450, yPercent:0, scale:1.5});
-gsap.set("#line-3",{transformOrigin: "50% 50%", xPercent:450, yPercent:0, scale:1.5});
-gsap.set("#line-4",{transformOrigin: "50% 50%", xPercent:450, yPercent:0, scale:1.5});
+gsap.set("#lines",{transformOrigin: "50% 50%", xPercent:450, yPercent:0, scale:1.5});
+gsap.set("#line-1",{transformOrigin: "50% 50%"});
+gsap.set("#line-2",{transformOrigin: "50% 50%"});
+gsap.set("#line-3",{transformOrigin: "50% 50%"});
+
 
 function starAnimation(){
     var tl = gsap.timeline();
-    tl.from("#line-1",{duration:1.25, y:"-300", scaleY:0})
-    tl.from("#line-2",{duration:1.25, y:"-300", scaleY:0}, "-=0.25")
-    tl.from("#line-3",{duration:1.25, y:"-300", scaleY:0}, "-=0.50")
-    tl.from("#line-4",{duration:1.25, y:"-300", scaleY:0}, "-=0.75")
-    .to("#line-2",{duration:0.25, rotate:45})
-    .to("#line-3",{duration:0.25, rotate:90},"-=0.25")
-    .to("#line-4",{duration:0.25, rotate:135}, "-=0.25")
+    tl.from("#lines",{duration:1.5, y:"-300", scaleY:0})
+    .to("#line-1",{duration:0.35, rotate:45})
+    .to("#line-2",{duration:0.35, rotate:90},"-=0.25")
+    .to("#line-3",{duration:0.35, rotate:135}, "-=0.25")
+    .to ("#half-line-1",{transformOrigin: "50% 100%", duration:0.35, rotate:45},"+=0.35")
+    .to ("#half-line-5",{transformOrigin: "50% 100%", duration:0.35, rotate:-45}, "-=0.35")
+    .to ("#half-line-1",{transformOrigin: "50% 50%", duration:0.35, y:74},"+=0.35")
+    .to ("#half-line-5",{transformOrigin: "50% 50%", duration:0.35, y:74},"-=0.35")
     
     
 
