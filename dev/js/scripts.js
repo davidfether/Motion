06749c639345feebbb5c01fa253c5a55502454ac
davidfether <102702088+davidfether@users.noreply.github.com>
2022-11-25 22:43:27 -0500

    lots-of-progress

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -6,7 +6,7 @@ import { CustomEase } from "gsap/CustomEase";
 
 gsap.registerPlugin(GSDevTools, DrawSVGPlugin, MotionPathPlugin,CustomEase);
 
-gsap.set("#star",{transformOrigin: "50% 50%", xPercent:400, yPercent:0, scale:1.5});
+gsap.set("#star",{transformOrigin: "50% 50%", xPercent:580, yPercent:0, scale:1.5});
 gsap.set("#line-1",{transformOrigin: "50% 50%"});
 gsap.set("#line-2",{transformOrigin: "50% 50%"});
 gsap.set("#line-3",{transformOrigin: "50% 50%"});
@@ -14,37 +14,84 @@ gsap.set("#line-3",{transformOrigin: "50% 50%"});
 
 function starAnimation(){
     var tl = gsap.timeline();
-    tl.from("#star",{duration:1.5, y:"-300", scaleY:0})
-    .to("#line-1",{duration:0.35, rotate:45})
-    .to("#line-2",{duration:0.35, rotate:90},"-=0.35")
-    .to("#line-3",{duration:0.35, rotate:135}, "-=0.35")
-    .to ("#half-line-1",{transformOrigin: "50% 100%", duration:0.35, rotate:45,fill:"#F2C31A"},"rotate")
-    .to ("#half-line-5",{transformOrigin: "50% 100%", duration:0.35, rotate:-45,fill:"#F2C31A"}, "rotate")
-    .to ("#half-line-3",{duration:0.35, fill:"#F2C31A"}, "rotate")
-    .to ("#half-line-yellow-1",{duration:0.35, x:52},"+=0.35")
-    .to ("#half-line-yellow-2",{duration:0.35, x:-52},"-=0.35")
+    tl.from("#star",{duration:1.25, y:"-300", scaleY:0})
+    .to("#line-1",{duration:1, rotate:45})
+    .to("#line-2",{duration:1, rotate:90},"-=1")
+    .to("#line-3",{duration:1, rotate:135}, "-=1")
+    .to ("#half-line-1",{transformOrigin: "50% 100%", duration:0.5, rotate:45,fill:"#E9C84A"},"rotate")
+    .to ("#half-line-5",{transformOrigin: "50% 100%", duration:0.5, rotate:-45,fill:"#E9C84A"}, "rotate")
+    .to ("#half-line-3",{duration:0.5, fill:"#E9C84A"}, "rotate")
+    .to ("#half-line-yellow-1",{duration:0.5, x:52},"+=0.5")
+    .to ("#half-line-yellow-2",{duration:0.5, x:-52},"-=0.5")
+    .fromTo ("#star",{duration:0.35, xPercent:125, yPercent:0}, {xPercent:-40, yPercent:0, scale:1})
 
     return tl;
 
+}
+
+ function typeAnimation(){
+    var tl = gsap.timeline();
+    tl.fromTo("#e",{duration:0.25, xPercent:500, yPercent:0, drawSVG: "0% 0%"}, {xPercent:0, yPercent:0, drawSVG: "0% 100%"},"+=0.25")
+    tl.fromTo("#v",{duration:0.25, xPercent:500, yPercent:100, drawSVG: "0% 0%"}, {xPercent:0, yPercent:0, drawSVG: "0% 100%"},"-=0.25")
+    tl.fromTo("#o",{duration:0.25, xPercent:100, yPercent:-200, drawSVG: "125% 125%"}, {xPercent:0, yPercent:0, drawSVG: "125% 250%"},"-=0.25")
+    tl.fromTo("#v-2",{duration:0.25, xPercent:100, yPercent:0, drawSVG: "0% 0%"}, {xPercent:0, yPercent:0, drawSVG: "0% 100%"},"-=0.25")
+    tl.fromTo("#l",{duration:0.25, xPercent:50, yPercent:0, drawSVG: "0% 0%"}, {xPercent:50, yPercent:0, drawSVG: "0% 100%"},"-=0.25")
+    tl.fromTo("#e-2",{duration:0.25, xPercent:100, yPercent:0, drawSVG: "0% 0%"}, {xPercent:0, yPercent:0, drawSVG: "0% 100%"},"-=0.25")
+    return tl;
 }
 
 
 function eAnimation(){
     var tl = gsap.timeline();
-    tl.from("o",{duration:0.25, scaleY:0})
-    to("#o",{duration:0.25, drawSVG: "75% 150%"})
-    
+  //  tl.fromTo("#e",{duration:0.25, xPercent:500, yPercent:0, drawSVG: "0% 0%"}, {xPercent:0, yPercent:0, drawSVG: "0% 110%"})
     return tl;
 }
 
+
 function vAnimation(){
     var tl = gsap.timeline();
-    tl.from("v",{duration:0.25, scaleY:0})
-    to("#v",{duration:0.25, drawSVG: "75% 150%"})
+    //tl.fromTo("#v",{duration:0.25, xPercent:500, yPercent:0, drawSVG: "0% 0%"}, {xPercent:0, yPercent:0, drawSVG: "0% 110%"},"-=0.25")
+
+    return tl;
+}
+
+function oAnimation(){
+    var tl = gsap.timeline();
+   // tl.fromTo("#o",{duration:0.25, drawSVG: "25% 25%"}, {drawSVG: "25% 150%"})
+    return tl;
+}
+
+function lAnimation(){
+    var tl = gsap.timeline();
+   // tl.fromTo("#l",{duration:0.25, drawSVG: "0% 0%"}, {drawSVG: "0% 110%"})
+
+    return tl;
+}
+
+
+function v2Animation(){
+    var tl = gsap.timeline();
+  //  tl.fromTo("#v-2",{duration:0.25, drawSVG: "0% 0%"}, {drawSVG: "0% 110%"})
+
+    return tl;
+}
+
+
+function e2Animation(){
+    var tl = gsap.timeline();
+    //tl.fromTo("#e-2",{duration:0.25, drawSVG: "0% 0%"}, {drawSVG: "0% 110%"})
+
+    return tl;
+}
 
 
 var mainTL = gsap.timeline();
 mainTL.add(starAnimation())
+.add(typeAnimation())
 .add(eAnimation())
 .add(vAnimation())
+.add(oAnimation())
+.add(lAnimation())
+.add(v2Animation())
+.add(e2Animation())
 GSDevTools.create();
