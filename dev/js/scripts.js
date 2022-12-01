86c776358e6ac908c9b45b4ce18ea96bf3565279
davidfether <102702088+davidfether@users.noreply.github.com>
2022-12-01 11:17:44 -0500

    thu-class

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -7,6 +7,7 @@ import { CustomEase } from "gsap/CustomEase";
 gsap.registerPlugin(GSDevTools, DrawSVGPlugin, MotionPathPlugin,CustomEase);
 
 gsap.set("#box-group",{transformOrigin: "center bottom", y:150, x:50, scale:1.5});
+gsap.set("#arch-1",{transformOrigin: "center bottom", y:-278});
 
 function boxAnimation(){
         var tl = gsap.timeline();
@@ -19,7 +20,7 @@ function boxAnimation(){
         .to("#box-group-8",{duration:0.25, x:60, y:30})
         .to("#box-group-9",{duration:0.25, x:60, y:60})
         .to("#box-10",{duration:0.25, x:-60, y:60})
-        .to("#box-group",{duration:0.5, x:25, scale:.6})
+        .to("#box-group",{duration:0.5, scale:.6})
 
 
         return tl;
@@ -29,20 +30,31 @@ function boxAnimation(){
 
 function lineAnimation(){
         var tl = gsap.timeline();
-        tl.to("#box-4",{duration:0.5, y:30, scaleY:-7},"+=0.5")
-        .to("#box-9",{duration:0.5, y:30, scaleY:-7},"-=0.5")
-        .to("#box-3",{duration:0.5, y:30, scaleY:-7},"+=0.5")
-        .to("#box-10",{duration:0.5, y:90, scaleY:-7},"-=0.5")
-        .to("#box-2",{duration:0.5, y:30, scaleY:-7},"+=0.5")
-        .to("#box-1",{duration:0.5, y:30, scaleY:-7},"-=0.5")
+
+        tl.to("#box-9",{duration:0.5, y:30, scaleY:-11})
+        .to("#box-10",{duration:0.5, y:90, scaleY:-11})
+        .to("#box-1",{duration:0.5, y:30, scaleY:-11})
+        .to("#box-2",{duration:0.5, y:30, scaleY:-11})
+        .to("#box-3",{duration:0.5, y:30, scaleY:-11})
+        .to("#box-4",{duration:0.5, y:30, scaleY:-11})
         
         return tl;
 }
+
+function archAnimation(){
+        var tl = gsap.timeline();
+        tl.from("#arch-1",{duration:0.5, drawSVG: "0% 0%"})
+
+        return tl;
+}
+
+
 
 
 
 var mainTL = gsap.timeline();
 mainTL.add(boxAnimation())
 .add(lineAnimation())
+.add(archAnimation())
 
 GSDevTools.create();
