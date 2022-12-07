82fbeca825efc85f50740a91ba237fed692de331
davidfether <102702088+davidfether@users.noreply.github.com>
2022-12-07 15:57:28 -0500

    redoing-assignment

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -1,91 +1,82 @@
 import { gsap } from "gsap";
-import { GSDevTools } from "gsap/GSDevTools";
 import { DrawSVGPlugin } from "gsap/DrawSVGPlugin";
-import { MotionPathPlugin } from "gsap/MotionPathPlugin";
-import { CustomEase } from "gsap/CustomEase";
+import { GSDevTools } from "gsap/GSDevTools";
 
-gsap.registerPlugin(GSDevTools, DrawSVGPlugin, MotionPathPlugin,CustomEase);
+gsap.registerPlugin(DrawSVGPlugin, GSDevTools);
 
-gsap.set("#t-down",{scaleY:0.5});
-gsap.set("#t-cross",{transformOrigin: "center"});
-gsap.set("#i",{transformOrigin: "center bottom"});
-gsap.set("#e",{transformOrigin: "left bottom"});
-gsap.set("#dot",{transformOrigin: "50% 50%", xPercent:-50, yPercent:-50});
+function simpleMotion(){
 
-function tAnimation(){
-        var tl = gsap.timeline();
-        tl.from("#t-down",{duration:0.5, y:"-=500", ease:"none"})
+      // make a var that represents the line in the SVG
+      var line = document.querySelector("#center-line");
+      // make a var that uses the svg line var from above and sets the BBox property
+      line = line.getBBox();
+      // print out the width of the line. This should make your Figma file
+      // console.log(line.width)
+  
+      var leftBall = document.querySelector("#left-ball");
+      leftBall = leftBall.getBBox();
+  
+      var center = document.querySelector("#center");
+      center = center.getBBox();
+      // console.log(center.height)
 
-        // stretch out bottom of T
-        .to("#t-down",{duration:0.5, scaleY:1.5, ease: "none)"})
+      gsap.set("#left-ball",{x:line.width / 2 + leftBall.width / 2, transformOrigin:"center" })
+      gsap.set("#right-ball",{x:-line.width/2 - leftBall.width / 2, transformOrigin:"center" })
 
-        // animate the bottom of the T path
-        .from("#t-path",{duration:0.25, scaleY:0, ease: "none)"},"-=20%")
-        .fromTo("#t-path",{duration:0.25, drawSVG: "0% 10%"}, {drawSVG:"90% 100%", ease:"none"}, "tAnimation")
+    var tl = gsap.timeline();
+    tl.from("#right-ball",{scale:0, duration:0.25, drawSVG: 0})
+      .from("#left-ball",{scale:0, duration:0.25},"-=50%")
+      .to("#right-ball",{x:0, duration:0.25},"rollOut")
+      .to("#left-ball",{x:0, duration:0.25},"rollOut")
+      .fromTo("#center-line",{drawSVG:"50% 50%"},{duration:0.25, drawSVG: "0% 100%"},"rollOut")
+      .from("#center",{duration: 0.25, scale:0, transformOrigin:"center"},"-=50%")
+      .to("#center",{duration: 0.25, y: -center.height / 2 - 50},"-=50%")
+      .to("#center",{duration: 0.15, y: 0})
+      .fromTo("#center-outline",{drawSVG:"100% 50%"},{duration:0.15,drawSVG:"125% 25%"},"-=50%")
 
-        // animate the t cross bar
-        .from("#t-cross",{duration:0.5, scaleX:0, ease: "elastic.out(1, 0.3)"}, "tAnimation")
+    return tl;
 
-        // snap T back into shape
-        .to("#t-down",{duration:0.5, scaleY:1, ease: "elastic.out(1, 0.3)"},"-=80%")
-        return tl;
 }
 
-function eAnimation(){
-        var tl = gsap.timeline();
-        //rotate E 
-        tl.from("#e", {duration:0.75, rotate: 120, ease: "elastic.out(1, 0.5)"})
-        return tl;
+function patternMotion(){
+
+    var tl = gsap.timeline();
+
+    tl.from(".odd",{duration:2, drawSVG:0, stagger:0.25, rotate:180, transformOrigin:"center"},"spin")
+    .fromTo(".even",{drawSVG:"0% 0%"},{duration:2, drawSVG:"0% -100%", stagger:0.25, rotate:180, transformOrigin:"center"},"spin");
+    return tl;
+
 }
 
-function kAnimation(){
-        var tl = gsap.timeline();
-        // drop in K
-        tl.from("#k", {duration:0.25, y:"-=200", ease: "bounce",autoAlpha:0})
-        return tl;
+function UIMotion(){
+
+
+    // make a var that represents the line in the SVG
+    var bottomLine = document.querySelector("#bottom-line");
+    // make a var that uses the svg line var from above and sets the BBox property
+    bottomLine = bottomLine.getBBox();
+
+    console.log(bottomLine.width);
+
+    var tl = gsap.timeline();
+    tl.to("#pencil",{duration:0.5,x:bottomLine.width},"draw")
+    .from("#bottom-line",{duration:0.5,drawSVG:0},"draw")
+    .to("#pencil",{duration:0.25, alpha:0},"drawOutline")
+    .fromTo("#outline",{drawSVG:"100% 100%"},{duration:0.5, drawSVG:"100% 0%"},"drawOutline")
+    .from("#box-bg",{duration:0.5, scaleX:0})
+    .from(".letter",{duration:0.25, alpha:0, y:20, stagger: 0.12},"-=50%")
+    .from("#glass",{duration:0.25, drawSVG:0},"-=.5")
+    .from("#handle",{duration:0.25, drawSVG:0},"-=.25")
+    return tl;
+
 }
 
-function nAnimation(){
-        var tl = gsap.timeline();
-        //skew the N in
-        tl.from("#n", {duration:0.25, scaleX:0})
-        return tl;
-}
-
-function iAnimation(){
-        var tl = gsap.timeline();
-        // make I pop out
-        tl.from("#i",{duration:0.5, scaleY:0, ease: "elastic.out(1, 0.5)"})
-        return tl;
-}
-
-function tPathAnimation(){
-        var tl = gsap.timeline();
-         // make T path disappear
-        tl.to("#t-path",{duration:0.25, scaleY:0})
-        return tl;
-}
-
-function dotAnimation(){
-        var tl = gsap.timeline();
-        // dot pops out and lands
-        tl.from("#dot",{duration:0.01, autoAlpha:0})
-        .from("#dot",{duration:1, motionPath:{path:"#dot-path", align:"#dot-path"},ease: "bounce.out"});
-
-        // .from("#dot",{duration:1, motionPath:{path:"#dot-path", align:"#dot-path"},ease: CustomEase.create("custom", "M0,0,C0.14,0,0.242,0.438,0.272,0.561,0.313,0.728,0.354,0.963,0.362,1,0.37,0.985,0.414,0.873,0.455,0.811,0.51,0.726,0.573,0.597,0.586,0.606,0.662,0.656,0.719,0.981,0.726,0.998,0.788,0.914,0.853,0.802,0.872,0.816,0.891,0.83,0.897,0.985,0.911,0.998,0.922,0.994,0.939,0.984,0.954,0.984,0.969,0.984,1,1,1,1")});
-
-        
-        return tl;
-}
 
 var mainTL = gsap.timeline();
-mainTL.add(tAnimation())
-.add(eAnimation(),"-=0.25")
-.add(kAnimation(),"-=0.6")
-.add(nAnimation(),"-=0.35")
-.add(iAnimation(),"-=0.6")
-.add(tPathAnimation(),"-=0.6")
-.add(dotAnimation(),"-=0.55")
+mainTL.add(simpleMotion())
+.add(patternMotion())
+.add(UIMotion())
+
 
 
 GSDevTools.create();
