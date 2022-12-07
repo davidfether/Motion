75cbb708a3cdc46d12327c8c63234d99c187830c
davidfether <102702088+davidfether@users.noreply.github.com>
2022-12-07 18:24:05 -0500

    added-files-again

---
+++ dev/js/scripts.js
@@ -0,0 +1,13 @@
+gsap.registerPlugin(DrawSVGPlugin, GSDevTools);
+
+function simpleMotion() {
+  var tl = gsap.timeline();
+  tl.from("#line-middle", {duration: 5, y:300, drawSVG: "0% 0%" })
+
+  return tl;
+}
+
+var mainTL = gsap.timeline();
+mainTL.add(simpleMotion())
+
+GSDevTools.create({animation: mainTL});
