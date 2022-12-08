ff7f5d2daf37dafd779c7f9dc7d7ce0624c68f35
davidfether <102702088+davidfether@users.noreply.github.com>
2022-12-08 15:53:27 -0500

    first-done

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -6,7 +6,17 @@ gsap.registerPlugin(DrawSVGPlugin, GSDevTools);
 
 function simpleMotion() {
   var tl = gsap.timeline();
-  tl.from("#circle-stroke", {duration: 5, y:300, drawSVG: "0% 0%" })
+  tl.to("#line-bottom", {duration: 1, y:-112},"+=0.25")
+  .to("#circle-stroke", {duration: 1, drawSVG: "50% 100%"})
+  tl.to("#line-middle", {duration: 1, y:112},"+=0.25")
+  .to("#circle-stroke", {duration: 1, drawSVG: "100% 150%"})
+  tl.to("#line-bottom", {duration: 1, y:10},"+=0.25")
+  .to("#circle-stroke", {duration: 1, drawSVG: "150% 200%"})
+  tl.to("#line-bottom", {duration: 1, y:-112},"+=0.25")
+  .to("#circle-stroke", {duration: 1, drawSVG: "200% 250%"})
+  tl.to("#line-bottom", {duration: 1, y:0},"+=0.25")
+  .to("#circle-stroke", {duration: 1, drawSVG: "200% 300%"})
+  tl.to("#line-middle", {duration: 1, y:-14},"+=0.25")
 
   return tl;
 }
